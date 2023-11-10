40fe363ee626fae08f6320b20b820880b704abb3
DitzieDoodle <128585593+DitzieDoodle@users.noreply.github.com>
2023-11-10 21:42:50 +0100

    add
    end scene and quick access canvas prefab

---
+++ Assets/Scripts/QuickSceneChanger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuickSceneChanger : MonoBehaviour
+{
+   public void LoadSceneOne()
+    {
+        SceneManager.LoadScene("Basic Gameplay");
+
+    }
+
+    public void LoadSceneTwo()
+    {
+        SceneManager.LoadScene("Crash");
+
+    }
+
+    public void LoadSceneThree()
+    {
+        SceneManager.LoadScene("Endgame");
+
+    }
+}
